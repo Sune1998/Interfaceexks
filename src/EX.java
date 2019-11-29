a465a1834eafc653c7c7a379a03ead5889449ab6
sune <sune.mozart@gmail.com>
2019-11-29 14:13:14 +0100

    added EX

---
+++ src/EX.java
@@ -0,0 +1,4 @@
+public interface EX {
+    int cool = 0;
+    public  void ex();
+}
--- src/ElectricPokemon.java
+++ src/ElectricPokemon.java
@@ -1,5 +1,10 @@
-public class ElectricPokemon extends Pokemon {
+public class ElectricPokemon extends Pokemon implements EX {
     public void spyild() {
         System.out.println("FireBall!");
+    }
+
+    @Override
+    public void ex() {
+        System.out.println("im cool bitch!");
     }
 }
